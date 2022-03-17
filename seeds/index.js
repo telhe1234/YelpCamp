edcfd9a65deac825dca5432c69ae0ce711cf39a3
Taha El-Helwani <telhe008@uottawa.ca>
2022-03-17 13:15:45 -0400

    Built the cmpgrounds CRUD, added basic styles, added errors handling and data validation, added the reviews model, restructured YelpCamp and added Flash to it, added in uthentication and started authorization

---
+++ seeds/index.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    // const c = new Campground({ title: 'purple field' });
+    // await c.save();
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground({
+            author: '62323c869e7648d4160df3ea',
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores porro veritatis quaerat totam sint! Sed provident sequi molestiae asperiores molestias quasi natus, doloribus est ipsam expedita nisi, amet odio fugiat?',
+            price
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
