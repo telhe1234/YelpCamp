d1b03b70f8983785beba7134a07e9975a5f03c48
Taha El-Helwani <telhe008@uottawa.ca>
2022-03-20 10:55:35 -0400

    Fixed formatting of files from 4 spaces to 2 spaces

--- controllers/users.js
+++ controllers/users.js
@@ -6,17 +6,17 @@ module.exports.renderRegister = (req, res) => {
 
 module.exports.register = async (req, res, next) => {
   try {
-      const { email, username, password } = req.body;
-      const user = new User({ email, username });
-      const registeredUser = await User.register(user, password);
-      req.login(registeredUser, err => {
-          if (err) return next(err);
-      })
-      req.flash('success', 'Welcome to Yelp Camp!');
-      res.redirect('/campgrounds');
+    const { email, username, password } = req.body;
+    const user = new User({ email, username });
+    const registeredUser = await User.register(user, password);
+    req.login(registeredUser, err => {
+      if (err) return next(err);
+    })
+    req.flash('success', 'Welcome to Yelp Camp!');
+    res.redirect('/campgrounds');
   } catch (e) {
-      req.flash('error', e.message);
-      res.redirect('register');
+    req.flash('error', e.message);
+    res.redirect('register');
   }
 };
 
