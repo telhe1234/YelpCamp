3e7c4baf25768f716bc189dcf05f6722e3b47f88
Taha El-Helwani <telhe008@uottawa.ca>
2022-03-21 10:55:42 -0400

    Integrated MapBox API into the app. It gives us the longitude and latitude which we can plot on a map later on

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,5 +1,9 @@
 const Campground = require('../models/campground');
-const {cloudinary} = require('../cloudinary')
+const { cloudinary } = require('../cloudinary');
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
+
 
 module.exports.index = async (req, res) => {
   const campgrounds = await Campground.find({});
@@ -11,6 +15,11 @@ module.exports.renderNewForm = (req, res) => {
 };
 
 module.exports.createCampground = async (req, res, next) => {
+  const geoData = await geocoder.forwardGeocode({
+    query: req.body.campground.location,
+    limit: 1
+  }).send();
+  res.send(geoData.body.features[0].geometry.coordinates);
   const campground = new Campground(req.body.campground);
   campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
   campground.author = req.user._id;
@@ -52,11 +61,11 @@ module.exports.updateCampground = async (req, res) => {
   const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
   campground.images.push(...imgs);
   await campground.save();
-  if(req.body.deleteImages) {
+  if (req.body.deleteImages) {
     for (let filename of req.body.deleteImages) {
       await cloudinary.uploader.destroy(filename);
     }
-    await campground.updateOne({$pull: {images: {filename: {$in: req.body.deleteImages}}}});
+    await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } });
     console.log(campground);
   }
   req.flash('success', 'Successfully updated campground!');
